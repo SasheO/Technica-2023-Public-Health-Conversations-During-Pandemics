90db1f321db805e738d0871477451e4cfd9922c9
SasheO <76496417+SasheO@users.noreply.github.com>
2023-10-21 22:50:25 -0400

    added scripts to run chatgpt

---
+++ llm/label_users_with_chatgpt.py
@@ -0,0 +1,48 @@
+import time
+import openai
+import pandas as pd
+from dotenv import load_dotenv, find_dotenv   # get the system environment variables
+import os  # get command line arguments
+
+filename = "../data/participant_data_for_subversive_tweets_on_vaccines.csv"
+f = open(filename, "r")
+userids = pd.read_csv(filename)['userid'].tolist()
+texts = pd.read_csv(filename)['text'].tolist()
+
+participants_to_tweets = {}
+
+for indx in range(len(userids)):
+    userid = userids[indx]
+    text = texts[indx]
+    if userid in participants_to_tweets:
+        if type(text)!=str:
+            continue
+        participants_to_tweets[userid] += "\n"+text
+    else:
+        participants_to_tweets[userid] = text
+
+load_dotenv(find_dotenv())  # load the environment variables
+openai.api_key=os.getenv("OPENAI_API_KEY")
+
+messages = []
+system_message = "You are deciding whether people are pro-science or anti-science with respect to COVID based on their tweets. Pro-science users generally express opinions that agree with conventional science and anti-science users may propagate conspiracies."
+messages.append({"role":"system","content":system_message})
+
+with open("chatgpt_classifications.csv", "a+") as f:
+    f.write("userid,classification\n")
+for user in participants_to_tweets:  # This starts an infinite loop
+    messages = []
+    system_message = "You are deciding whether people are pro-science or anti-science with respect to COVID based on their tweets. Pro-science users generally express opinions that agree with conventional science and anti-science users may propagate conspiracies."
+    messages.append({"role":"system","content":system_message})
+    message = "I am trying to classify twitter users as either pro-science or anti-science with respect to COVID. Pro-science users generally express opinions that agree with conventional science and anti-science users may propagate conspiracies. Could you classify a user based on this text. Your output should simply be pro-science, anti-science or unsure: "+participants_to_tweets[user]   
+    messages.append({"role":"user","content": message})
+
+    response=openai.ChatCompletion.create(
+     model="gpt-4",
+     messages=messages
+    )
+    time.sleep(60)
+
+    reply = response["choices"][0]["message"]["content"]
+    with open("chatgpt_classifications.csv", "a+") as f:
+        f.write(str(user)+","+reply+"\n")
---
+++ llm/label_users_with_chatgpt_interactive.py
@@ -0,0 +1,29 @@
+import openai
+from dotenv import load_dotenv, find_dotenv   # get the system environment variables
+import os  # get command line arguments
+
+load_dotenv(find_dotenv())  # load the environment variables
+openai.api_key=os.getenv("OPENAI_API_KEY")
+
+messages = []
+system_message = "You are deciding whether people are pro-science or anti-science based on their tweets"
+messages.append({"role":"system","content":system_message})
+
+print("Alright! I am ready to be your advisor chatbot" + "\n")
+
+while True:  # This starts an infinite loop
+    user_input = input("What should I label: (type 'exit' to quit): ")
+    if user_input.lower().strip() == "exit":  # Convert the input to lowercase for a case-insensitive comparison
+        break  # This will exit the loop if "exit" is typed
+
+    message = user_input    
+    messages.append({"role":"user","content": message})
+
+    response=openai.ChatCompletion.create(
+     model="gpt-4",
+     messages=messages
+    )
+
+    reply = response["choices"][0]["message"]["content"]
+    print(reply)
+print("Bye!!!")
