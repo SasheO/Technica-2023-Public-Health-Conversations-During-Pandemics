217d5628d6b03e040dcd30cb5bed55f6b0d43587
SasheO <76496417+SasheO@users.noreply.github.com>
2023-10-21 23:35:41 -0400

    ensured encodings do not exceed rate

--- llm/label_users_with_chatgpt.py
+++ llm/label_users_with_chatgpt.py
@@ -2,6 +2,7 @@ import time
 import openai
 import pandas as pd
 from dotenv import load_dotenv, find_dotenv   # get the system environment variables
+import tiktoken
 import os  # get command line arguments
 
 filename = "../data/participant_data_for_subversive_tweets_on_vaccines.csv"
@@ -28,13 +29,25 @@ messages = []
 system_message = "You are deciding whether people are pro-science or anti-science with respect to COVID based on their tweets. Pro-science users generally express opinions that agree with conventional science and anti-science users may propagate conspiracies."
 messages.append({"role":"system","content":system_message})
 
+encoding = tiktoken.encoding_for_model("gpt-3.5-turbo") 
+
+# a = 0
 with open("chatgpt_classifications.csv", "a+") as f:
     f.write("userid,classification\n")
 for user in participants_to_tweets:  # This starts an infinite loop
     messages = []
     system_message = "You are deciding whether people are pro-science or anti-science with respect to COVID based on their tweets. Pro-science users generally express opinions that agree with conventional science and anti-science users may propagate conspiracies."
     messages.append({"role":"system","content":system_message})
-    message = "I am trying to classify twitter users as either pro-science or anti-science with respect to COVID. Pro-science users generally express opinions that agree with conventional science and anti-science users may propagate conspiracies. Could you classify a user based on this text. Your output should simply be pro-science, anti-science or unsure: "+participants_to_tweets[user]   
+    message = "I am trying to classify twitter users as either pro-science or anti-science with respect to COVID. Pro-science users generally express opinions that agree with conventional science and anti-science users may propagate conspiracies. Could you classify a user based on this text. Your output should simply be pro-science, anti-science or unsure: \n"+participants_to_tweets[user]   
+    token_count = len(encoding.encode(message))
+    if token_count > 4097:
+        message_list = message.split()
+        current = 1
+        
+    while token_count > 4097:
+        message = ""
+        for x in message_list[:-current]:
+            message += x + "\n"
     messages.append({"role":"user","content": message})
 
     response=openai.ChatCompletion.create(
@@ -44,5 +57,12 @@ for user in participants_to_tweets:  # This starts an infinite loop
     time.sleep(60)
 
     reply = response["choices"][0]["message"]["content"]
-    with open("chatgpt_classifications.csv", "a+") as f:
-        f.write(str(user)+","+reply+"\n")
+    try:
+        with open("chatgpt_classifications.csv", "a+") as f:
+            f.write(str(user)+","+reply+"\n")
+    except:
+        with open("chatgpt_classifications.csv", "a+") as f:
+            f.write(str(user)+","+reply+"\n")
+#         a += 1
+#         if a == 2:
+#             break
